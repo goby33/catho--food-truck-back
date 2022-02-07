21fe781056958ab5b1e0156bca442b09ad6a515e
Martin <martin.gobert@adeo.com>
2022-02-07 17:38:48 +0100

    first commit

---
+++ src/main/java/com/example/cathoappfoodtruckback/CathoAppFoodTruckBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cathoappfoodtruckback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathoAppFoodTruckBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CathoAppFoodTruckBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cathoappfoodtruckback/CathoAppFoodTruckBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cathoappfoodtruckback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CathoAppFoodTruckBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
