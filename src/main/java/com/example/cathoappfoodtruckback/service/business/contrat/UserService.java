8a1c2f91b09167c0fbd35aa7d566ccfcd219fe56
Martin <martin.gobert@adeo.com>
2022-02-15 10:59:50 +0100

    add user

--- src/main/java/com/example/cathoappfoodtruckback/service/business/contrat/UserService.java
+++ src/main/java/com/example/cathoappfoodtruckback/service/business/contrat/UserService.java
@@ -1,4 +1,8 @@
 package com.example.cathoappfoodtruckback.service.business.contrat;
 
+import com.example.cathoappfoodtruckback.service.api.model.UserDTO;
+import com.example.cathoappfoodtruckback.service.exception.FunctionalException;
+
 public interface UserService {
+    public UserDTO getUser(String name, String mdp) throws FunctionalException;
 }
